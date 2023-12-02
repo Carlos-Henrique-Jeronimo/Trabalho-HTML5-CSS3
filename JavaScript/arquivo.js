7dbee7f9665a10458ccdf1a8e23a990fa7f43c40
Carlos-Henrique-Jeronimo <152661539+Carlos-Henrique-Jeronimo@users.noreply.github.com>
2023-12-02 09:59:05 -0300

    Create arquivo.js
    JavaScript

---
+++ JavaScript/arquivo.js
@@ -0,0 +1,48 @@
+function openNav(){
+    document.getElementById("myNav").style.width = '100%';
+}
+function closeNav(){
+    document.getElementById("myNav").style.width = '0%';
+}
+function gabNav(){
+    document.getElementById("gaboverlay").style.width='100%';
+}
+function placNav(){
+    document.getElementById("placoverlay").style.width= '100%';
+}
+function hdNav(){
+    document.getElementById("hdoverlay").style.width='100%';
+}
+function ramNav(){
+    document.getElementById("ramoverlay").style.width='100%';
+}
+function procsNav(){
+    document.getElementById("procsoverlay").style.width='100%';
+}
+function gabsNav(){
+    document.getElementById("gabsoverlay").style.width='100%';
+}
+function cuidNav(){
+    document.getElementById("cuidoverlay").style.width='100%';
+}
+function persNav(){
+    document.getElementById("persoverlay").style.width='100%';
+}
+function modelNav(){
+    document.getElementById("modeloverlay").style.width= '100%';
+}
+function img2Nav(){
+    document.getElementById("imgoverlay").style.width= '70%';
+}
+function img1Nav(){
+    document.getElementById("imgoverla").style.width= '30%';
+}
+function imgNav(){
+    document.getElementById("imggab").style.width= '30%';
+}
+function placsNav(){
+    document.getElementById("placsoverlay").style.width= '100%';
+}
+function equiNav(){
+    document.getElementById("equioverlay").style.width= '100%';
+}
